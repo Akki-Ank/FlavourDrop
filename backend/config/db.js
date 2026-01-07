3af0332a5e2fb813c09aee5724af2134baea60f9
Ankit Deb <ankitdeb1111@gmail.com>
2026-01-08 03:23:25 +0530

    Done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
-export const connectDB = async()=>{
-    await mongoose.connect(`mongodb+srv://akkid98:Darkshadow@cluster0.u7djkac.mongodb.net/FlavourDrop?appName=Cluster0`).then(()=>console.log("DB Connected"));
-
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("DB Error:", error.message);
+    process.exit(1);
+  }
+};
